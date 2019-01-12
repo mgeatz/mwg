7d45636a586214772d9d9c6c4bd5f193579a5d2e
mgeatz <mgeatz01@gmail.com>
2019-01-11 23:57:28 -0600

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ResumeComponent} from "./resume/resume.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'resume'
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
