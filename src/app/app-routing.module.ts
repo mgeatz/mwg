60438cd3dff9170fb13b1f1627a8ba8801572d25
mgeatz <mgeatz01@gmail.com>
2019-07-13 22:17:00 -0500

    new homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ResumeComponent} from "./resume/resume.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'resume'
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'resume',
